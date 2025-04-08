53c4e615e064f1a731d44ee3028b013e9424b2f3
oxyum services <29GariglioSe@fortcherry.org>
2025-04-08 13:41:26 -0400

    push

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const puppeteer = require("puppeteer");
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 const browser = await puppeteer.browser();
 const page = await browser.newPage();
 
 await page.goto("https://developers.google.com/");
 await page.setViewport({width: 1080, height: 1024});
 
-
+app.listen(PORT, () => {
+    console.log("backend running on port 8080");
+});
 await browser.close();
