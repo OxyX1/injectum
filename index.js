810a7e7f2ae2c7440bba51031ea6c2d865baedf1
oxyum services <29GariglioSe@fortcherry.org>
2025-04-08 13:39:31 -0400

    push

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-const express = require('express');
-const axios = require('axios');
-const NodeCache = require('node-cache');
+const express = require("express");
+const puppeteer = require("puppeteer");
 
 const app = express();
-const cache = new NodeCache({ stdTTL: 3600 }); // Cache TTL: 1 hour
 
-// Use the dynamically assigned port from Railway or default to 39031
-const PORT = process.env.PORT || 39031;
+const browser = await puppeteer.browser();
+const page = await browser.newPage();
 
-app.get('/proxy', async (req, res) => {
-    const targetUrl = req.query.result;
-    if (!targetUrl) {
-        return res.status(400).send("Missing 'result' query parameter!");
-    }
+await page.goto("https://developers.google.com/");
+await page.setViewport({width: 1080, height: 1024});
 
-    // Check if the response is cached
-    if (cache.has(targetUrl)) {
-        console.log("Serving cached content");
-        return res.send(cache.get(targetUrl));
-    }
 
-    try {
-        // Fetch the target URL content
-        const response = await axios.get(targetUrl);
-        cache.set(targetUrl, response.data); // Store the response in the cache
-        console.log("Serving fresh content");
-        res.set('Content-Type', response.headers['content-type']);
-        return res.send(response.data);
-    } catch (error) {
-        console.error(`Error fetching URL: ${error.message}`);
-        return res.status(500).send("Failed to fetch target URL");
-    }
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Proxy server running at http://localhost:${PORT}/`);
-});
+await browser.close();
