da371c6055098a0eb6720228510b294f956672bc
oxyum services <29GariglioSe@fortcherry.org>
2025-04-08 12:36:27 -0400

    push

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from cachetools import TTLCache
 import os
 
 app = Flask(__name__)
-cache = TTLCache(maxsize=100, ttl=3600)  # Cache with 1-hour TTL
+cache = TTLCache(maxsize=100, ttl=3600)  # Cache with a 1-hour TTL
 
 @app.route('/proxy')
 def proxy():
@@ -12,13 +12,13 @@ def proxy():
     if not target_url:
         return "Missing 'result' query parameter!", 400
 
-    # Check if cached
+    # Check cache
     if target_url in cache:
         print("Serving cached content")
         return cache[target_url]
 
     try:
-        # Forward request to the target URL
+        # Forward the request to the target URL
         response = requests.get(target_url)
         cache[target_url] = Response(response.content, content_type=response.headers.get('Content-Type'))
         return Response(response.content, content_type=response.headers.get('Content-Type'))
@@ -27,5 +27,5 @@ def proxy():
         return "Failed to fetch target URL", 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 39031))  # Use environment variable or default to 39031
+    port = int(os.environ.get('PORT', 39031))  # Use PORT env variable or default to 39031
     app.run(host="0.0.0.0", port=port)
