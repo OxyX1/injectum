2404b7dea4dfc8709d1eefc5d33dc1dd75be81f1
oxyum services <29GariglioSe@fortcherry.org>
2025-04-10 08:12:13 -0400

    function setup

---
+++ injectum/injectum.py
@@ -0,0 +1,18 @@
+import pymem
+
+class injectum:
+    class internal:
+        pass
+
+    class external:
+        def __init__(self):
+            pass
+
+        def pidID(pid):
+            pass
+
+        def ReadMem():
+            pass
+        
+        def WriteMem():
+            pass
