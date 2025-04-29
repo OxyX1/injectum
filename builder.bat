79f436a654c4aa88937a25803a88cdda2caa7f83
oxyum services <29GariglioSe@fortcherry.org>
2025-04-29 04:32:32 +0000

    push

--- builder.bat
+++ builder.bat
@@ -1,6 +1,4 @@
 @echo off
 
-
-echo [INFO] trying to build exe [INFO]
-x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -Iinclude -Isrc -Iimgui -Iimgui/backends src/*.cpp -o injectum.exe -Llib-mingw-w64 -lglfw3 -lopengl32 -lgdi32 -luser32
+echo THIS FILE IS UNDER DEVELOPEMENT AS OF NOW PLEASE USE THE PREBUILT EXE
 pause
---
+++ instructions.md
@@ -0,0 +1,3 @@
+how to use.
+
+you can delete the source code if you want and keep the injectum.exe
