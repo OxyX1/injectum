daca9ae7f81c1f0692d5085d4e8a07d0d9ff5a31
oxyum services <29GariglioSe@fortcherry.org>
2025-04-29 03:05:45 +0000

    setup fix

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os
+import subprocess
 
 # Make setup.sh executable
-os.system('chmod +x script/setup.sh')
+subprocess.run('chmod +x script/setup.sh', shell=True, check=True)
 
 # Run setup.sh first!
-os.system('./script/setup.sh')
+subprocess.run('./script/setup.sh', shell=True, check=True)
 
-# THEN compile after setup
-os.system('g++ -Iinclude -Isrc -Iimgui/backends -Iimgui src/*.cpp -o injectum -lglfw -lGL -ldl -lpthread')
+# Compile C++ code
+subprocess.run('g++ -Iinclude -Isrc -Iimgui/backends -Iimgui src/*.cpp -o injectum -lglfw -lGL -ldl -lpthread', shell=True, check=True)
 
-# Now run your main Python program
-os.system('python3 main.py')
+# Now run your Python program (optional, if you have one)
+subprocess.run('python3 main.py', shell=True, check=True)
